d9a194a0ccd9d8ae1901bf516311bf7c925daa12
sheriff <ajibolasheriffdeen02@gmail.com>
2024-10-23 23:33:23 +0100

    business offer section completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import BusinessOffer from "@/components/business-offer";
 import Hero from "@/components/hero";
+import HomeCard from "@/components/home-card";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <Hero />
+      <BusinessOffer />
     </main>
   );
 }
