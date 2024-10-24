572112a0e9372b866f8a1921d14dc78998ad5c67
Lukman10a <abdulrauflukman9@gmail.com>
2024-10-24 08:16:03 +0100

    removed an extra testimonial from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import BusinessOffer from "@/components/business-offer";
 import SecurityCertifications from "@/components/certifications";
 import Hero from "@/components/hero";
 import Testimonials from "@/components/what-client";
-// import KeyBenefits from "@/components/why-choose";
 import FAQSection from "@/components/FAQ";
 
 import WhyChooseUs from "@/components/why-choose-us";
@@ -11,9 +10,7 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-8">
       <Hero />
-      {/* <KeyBenefits /> */}
       <SecurityCertifications />
-      <Testimonials />
       <BusinessOffer />
       <WhyChooseUs />
       <SecurityCertifications />
