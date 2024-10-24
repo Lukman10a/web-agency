1e062b7a765def28dfc1e3824b36850b904f12de
Lukman10a <abdulrauflukman9@gmail.com>
2024-10-24 08:01:06 +0100

    fixed a conflict in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,23 @@ import BusinessOffer from "@/components/business-offer";
 import SecurityCertifications from "@/components/certifications";
 import Hero from "@/components/hero";
 import Testimonials from "@/components/what-client";
-
-import SecurityCertifications from "@/components/certifications";
-import KeyBenefits from "@/components/why-choose";
+// import KeyBenefits from "@/components/why-choose";
 import FAQSection from "@/components/FAQ";
 
 import WhyChooseUs from "@/components/why-choose-us";
-
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-8">
       <Hero />
-      <KeyBenefits />
+      {/* <KeyBenefits /> */}
       <SecurityCertifications />
       <Testimonials />
-      <FAQSection />
-
       <BusinessOffer />
       <WhyChooseUs />
       <SecurityCertifications />
       <Testimonials />
+      <FAQSection />
     </main>
   );
 }
