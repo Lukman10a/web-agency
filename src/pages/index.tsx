210104152f8371ca75f1d3c799e2db1f187fcbe2
luqmaan <obalolaluqman16@gmail.com>
2024-10-24 07:37:12 +0100

    added the faq section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/hero";
 import Testimonials from "@/components/what-client";
 import SecurityCertifications from "@/components/certifications";
 import KeyBenefits from "@/components/why-choose";
+import FAQSection from "@/components/FAQ";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <KeyBenefits />
       <SecurityCertifications />
       <Testimonials />
+      <FAQSection />
     </main>
   );
 }
