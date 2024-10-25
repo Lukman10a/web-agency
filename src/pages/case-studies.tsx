bdc9c35c009834968d3ab8878969eaea26635110
Lukman10a <abdulrauflukman9@gmail.com>
2024-10-25 19:31:33 +0100

    create case study hero

--- src/pages/case-studies.tsx
+++ src/pages/case-studies.tsx
@@ -1,7 +1,35 @@
+import ArrowIcon from "@/components/icons/arrow";
+import { Button } from "@/components/ui/button";
+import caseStudyHero from "../../public/assets/case-study-hero.png";
 import React from "react";
+import Image from "next/image";
 
 const CaseStudies = () => {
-  return <div>CaseStudies</div>;
+  return (
+    <div>
+      <section className="flex md:flex-col items-center gap-10 bg-gradient-to-r from-[rgba(8,19,72,0.1)] to-[rgba(255,149,87,0.1)] p-12">
+        <article className="space-y-10">
+          <h1 className="font-sora font-extrabold text-6xl animate-fade-in-up">
+            Behind the Clouds
+          </h1>
+          <p className="font-medium text-xl animate-fade-in-up delay-200 font-sans">
+            We make sure you receive a tailor-made & cost efficient cloud
+            environment that is secure, scalable, easy to operate and
+            built-to-last.
+          </p>
+          <Button className="border border-[#081348] flex gap-4 items-center bg-orange-650 w-fit rounded-2xl text-white transition duration-500 transform hover:scale-105 hover:bg-orange-600">
+            <span>EXPLORE</span>
+            <ArrowIcon />
+          </Button>
+        </article>
+        <Image
+          src={caseStudyHero}
+          alt="case-study-hero"
+          className="animate-fade-in-up delay-500"
+        />
+      </section>
+    </div>
+  );
 };
 
 export default CaseStudies;
