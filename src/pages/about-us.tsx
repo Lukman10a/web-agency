2b91ca9add40e0dce6391a99b5aaef1ebd82cf9e
Lukman10a <abdulrauflukman9@gmail.com>
2024-10-25 13:31:47 +0100

    create about us hero

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -8,25 +8,23 @@ const AboutUs = () => {
   return (
     <Fragment>
       <section className="">
-        <section className="absolute">
-          <div className="p-16 text-center flex flex-col items-center gap-4 bg-gradient-to-r from-[rgba(8,19,72,0.1)] to-[rgba(255,149,87,0.1)] p-12s">
-            <h1 className="font-sora font-extrabold text-6xl animate-fade-in-up">
-              <span className="block">Behind the </span>
-              <span className="block">clouds</span>
-            </h1>
-            <p className="px-36 font-medium text-xl animate-fade-in-up delay-200 font-sans">
-              We make sure you receive a tailor-made & cost-efficient cloud
-              environment that is secure, scalable, easy to operate, and
-              built-to-last
-            </p>
-            <Button className="border border-[#081348] flex gap-4 items-center bg-orange-650 w-fit rounded-2xl text-white transition duration-500 transform hover:scale-105 hover:bg-orange-600">
-              <span>EXPLORE</span>
-              <ArrowIcon />
-            </Button>
-          </div>
-        </section>
-        <ImagesReveal />
+        <div className="p-16 text-center flex flex-col items-center gap-4 bg-gradient-to-r from-[rgba(8,19,72,0.1)] to-[rgba(255,149,87,0.1)] p-12s">
+          <h1 className="font-sora font-extrabold text-6xl animate-fade-in-up">
+            <span className="block">Behind the </span>
+            <span className="block">clouds</span>
+          </h1>
+          <p className="px-36 font-medium text-xl animate-fade-in-up delay-200 font-sans">
+            We make sure you receive a tailor-made & cost-efficient cloud
+            environment that is secure, scalable, easy to operate, and
+            built-to-last
+          </p>
+          <Button className="border border-[#081348] flex gap-4 items-center bg-orange-650 w-fit rounded-2xl text-white transition duration-500 transform hover:scale-105 hover:bg-orange-600">
+            <span>EXPLORE</span>
+            <ArrowIcon />
+          </Button>
+        </div>
       </section>
+      <ImagesReveal />
       <OurValues />
     </Fragment>
   );
