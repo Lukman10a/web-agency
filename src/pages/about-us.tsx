f59074b67bc266f1817a0d1e73781d9b4030a703
Abdulwasiu Abdulmuize <54303765+AOMuiz@users.noreply.github.com>
2024-10-25 09:11:10 +0100

    image reveal

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,9 +1,11 @@
+import ImagesReveal from "@/components/shared/image-reveal";
 import OurValues from "@/components/shared/our-values";
 import React, { Fragment } from "react";
 
 const AboutUs = () => {
   return (
     <Fragment>
+      <ImagesReveal />
       <OurValues />
     </Fragment>
   );
