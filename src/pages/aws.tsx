d44b9a4e07cfc1af2f7f4fa5901116adafa2e856
Lukman10a <abdulrauflukman9@gmail.com>
2024-10-29 21:36:34 +0100

    create /eks-solution page

--- src/pages/aws.tsx
+++ src/pages/aws.tsx
@@ -19,6 +19,7 @@ import ContactSection from "@/components/lets-talk";
 import CategoryButtons from "@/components/shared/category-buttons";
 import AWSection from "@/components/AWSection";
 import AwsLara from "@/components/AwsLara";
+import LARA from "@/components/shared/lara";
 
 export default function AWS() {
   const cardData = [
@@ -57,50 +58,6 @@ export default function AWS() {
     "Co-funding with AWS Migrations Acceleration Program (MAP)",
   ];
 
-  interface Lara {
-    title: string;
-    quote: string;
-    image: string;
-  }
-
-  const lara: Lara[] = [
-    {
-      image: black_mark,
-      title: "AWS Organization and access management",
-      quote:
-        "Centrally govern your all environments, manage access rights, permissions and policies.",
-    },
-    {
-      image: black_mark,
-      title: "VPC and related services",
-      quote:
-        "Network foundation, app and edge networking, hybrid connectivity and security.",
-    },
-    {
-      image: black_mark,
-      title: "AWS Organization and access management",
-      quote:
-        "Centrally govern your all environments, manage access rights, permissions and policies.",
-    },
-    {
-      image: black_mark,
-      title: "VPC and related services",
-      quote:
-        "Network foundation, app and edge networking, hybrid connectivity and security.",
-    },
-    {
-      image: black_mark,
-      title: "Kubernetes",
-      quote:
-        "Operate kubernetes on your own infrastructure, experience ultimate scaling and cost savings.",
-    },
-    {
-      image: black_mark,
-      title: "Observability",
-      quote:
-        "Grafana, Loki, Thanos, Tempo, Opensearch, Elastic. You name it, we have it.",
-    },
-  ];
 
   return (
     <div>
@@ -234,43 +191,7 @@ export default function AWS() {
       </section>
       <AWSection />
       <AwsLara />
-      <section className="bg-white p-4">
-        <section className="rounded-xl bg-gradient-to-r from-gradient-100 to-gradient-200 py-16">
-          <div className="mx-auto max-w-7xl px-4 lg:px-8 sm:px-6">
-            <div className="text-center">
-              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-                LARA building blocks{" "}
-              </h2>
-              <p className="mt-4 text-lg text-gray-600">
-                All of the componentes mentioned below are defined using
-                Infrastructure as Code, allowing for further customization and
-                reusability in different environments.{" "}
-              </p>
-            </div>
-
-            <div className="mt-10 grid grid-cols-2 gap-8 md:grid-cols-2 sm:grid-cols-1">
-              {lara.map((item, index) => (
-                <article
-                  key={index}
-                  className="space-y-4 rounded-lg bg-white p-6 shadow-md transition-shadow duration-300 hover:shadow-lg"
-                >
-                  <Image src={black_mark} alt="black_mark" />
-                  <h3 className="text-2xl font-bold text-[#262626]">
-                    {item.title}
-                  </h3>
-                  <p className="text-[#262626]">{item.quote}</p>
-                </article>
-              ))}
-            </div>
-          </div>
-          <div className="flex justify-center pt-10">
-            <Button className="flex w-fit transform items-center gap-4 rounded-2xl border border-[#081348] bg-orange-650 text-white transition duration-500 hover:scale-105 hover:bg-orange-600">
-              <span>SEE MORE</span>
-              <ArrowIcon />
-            </Button>
-          </div>
-        </section>
-      </section>
+     <LARA/>
 
       <div className="bg-white p-4">
         <div className="rounded-xl bg-gradient-to-r from-gradient-100 to-gradient-200">
---
+++ src/pages/eks-solution.tsx
@@ -0,0 +1,116 @@
+import AwsLara from "@/components/AwsLara";
+import ContactSection from "@/components/lets-talk";
+import CallToAction from "@/components/shared/callToAction";
+import LARA from "@/components/shared/lara";
+import Image from "next/image";
+import black_mark from "../../public/svg/black_mark.svg";
+import React from "react";
+
+// Benefit Component
+const Benefit = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => (
+  <div className="flex flex-col gap-4 p-4">
+    <Image src={black_mark} alt="black_mark" />
+    <h3 className="text-lg font-bold">{title}</h3>
+    <p className="text-gray-600">{description}</p>
+  </div>
+);
+
+export default function EKSSolution() {
+  const benefitItems = [
+    {
+      title: "Managed Service",
+      description: "AWS takes care of your underlying infrastructure",
+    },
+    {
+      title: "Super Scalable",
+      description: "Easily scale your apps to meet your changing demands",
+    },
+    {
+      title: "Highly Available",
+      description:
+        "Built-in redundancy and fault tolerance ensuring uninterrupted availability for your customers",
+    },
+  ];
+
+  return (
+    <section>
+      <section>
+        <div className="flex flex-col items-center space-y-6 p-20 text-center md:p-12 sm:p-4">
+          <p className="cursor-pointer rounded-full border border-[#808080] px-3 py-2 font-sans text-lg text-[#808080] sm:text-base">
+            AWS
+          </p>
+          <h1 className="text-5xl md:text-4xl sm:text-3xl">
+            Amazon Elastic Kubernetes Service
+          </h1>
+          <p>
+            Super scalable and reliable, that’s one way to describe EKS –
+            Elastic Kubernetes Service. EKS is a go-to choice for deploying &
+            managing containerized applications in the cloud, enabling you to
+            focus on building and running your applications rather than worrying
+            about underlying infrastructure.
+          </p>
+        </div>
+      </section>
+
+      <section className="mx-auto max-w-6xl space-y-8 p-8">
+        <h2 className="text-xl font-semibold text-orange-600">Benefits</h2>
+        <div className="grid grid-cols-3 gap-6 md:grid-cols-2 sm:grid-cols-1">
+          {benefitItems.map((benefit, index) => (
+            <Benefit
+              key={index}
+              title={benefit.title}
+              description={benefit.description}
+            />
+          ))}
+          {benefitItems.map((benefit, index) => (
+            <Benefit
+              key={index + 3}
+              title={benefit.title}
+              description={benefit.description}
+            />
+          ))}
+        </div>
+
+        <h2 className="text-xl font-semibold text-orange-600">Benefits</h2>
+        <div className="grid grid-cols-3 gap-6 md:grid-cols-2 sm:grid-cols-1">
+          {benefitItems.map((benefit, index) => (
+            <Benefit
+              key={index + 6}
+              title={benefit.title}
+              description={benefit.description}
+            />
+          ))}
+          {benefitItems.map((benefit, index) => (
+            <Benefit
+              key={index + 9}
+              title={benefit.title}
+              description={benefit.description}
+            />
+          ))}
+        </div>
+
+        <h2 className="mt-8 text-lg font-bold">Our Experience</h2>
+        <p className="text-gray-600">
+          We heavily utilise EKS in most of our customer infrastructures. It
+          allows them to scale their services to virtually infinite proportions,
+          accommodate spikes in load and make the whole setup as efficient as
+          possible. High availability, reliability, scalability, and cost
+          efficiency are the reasons we choose EKS to host our customer
+          services.
+        </p>
+      </section>
+      <div className="flex justify-center lg:px-6">
+        <CallToAction />
+      </div>
+      <AwsLara />
+      <LARA />
+      <ContactSection />
+    </section>
+  );
+}
