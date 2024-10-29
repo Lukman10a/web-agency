62db06dd9db4037e60d86dc03b73c28f5477f034
Lukman10a <abdulrauflukman9@gmail.com>
2024-10-29 09:47:26 +0100

    update the faq-section

--- src/components/shared/faq-section.tsx
+++ src/components/shared/faq-section.tsx
@@ -44,7 +44,7 @@ const FAQSection: React.FC<FAQSectionProps> = ({
                 </h3>
 
                 <button
-                  className={`ml-4 flex h-8 w-8 sm:h-6 sm:w-6 items-center justify-center rounded-full transition-colors ${
+                  className={`ml-4 flex h-8 w-8 sm:w-10 items-center justify-center rounded-full transition-colors ${
                     activeIndex === index
                       ? "bg-darkblue-900 text-white"
                       : "bg-gray-200 text-blue-600"
