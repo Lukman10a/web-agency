52bd1c2691676a6ea45b50954cc11185edadb385
Lukman10a <abdulrauflukman9@gmail.com>
2024-10-31 16:53:18 +0100

    change

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,9 +9,9 @@ import FlickeringGrid from "./ui/flickering-grid";
 const Hero = () => {
   return (
     <Fragment>
-      <header className="isolate w-11/12 overflow-hidden relative bg-darkblue-950 bg-cover rounded-xl max-w-7xl md:px-4 px-10 py-28 mx-auto md:w-full md:rounded-none">
+      <header className="relative isolate mx-auto w-11/12 max-w-7xl overflow-hidden rounded-xl bg-darkblue-950 bg-cover px-10 py-28 md:w-full md:rounded-none md:px-4">
         <FlickeringGrid
-          className="z-0 absolute inset-0 size-full "
+          className="absolute inset-0 z-0 size-full"
           squareSize={4}
           gridGap={6}
           color="#6B7280"
@@ -21,11 +21,11 @@ const Hero = () => {
           height={800}
         />
         <>
-          <div className="text-center z-[1] relative">
-            <h1 className="tracking-tight font-sora font-extrabold text-white md:text-3xl text-6xl">
+          <div className="relative z-[1] text-center">
+            <h1 className="font-sora text-6xl font-extrabold tracking-tight text-white md:text-3xl">
               <WaveReveal
                 mode="word"
-                className="text-foreground overflow-hidden"
+                className="overflow-hidden text-foreground"
                 text="SECURE YOUR CLOUD,"
               />
               <WaveReveal
@@ -34,15 +34,15 @@ const Hero = () => {
                 text="PROTECT YOUR BUSINESS"
               />
             </h1>
-            <p className="mt-3 font-mono text-base text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            <p className="mt-3 font-mono text-base text-gray-300 lg:mx-0 md:mt-5 md:text-xl sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg">
               We provide expert consulting services to help businesses achieve
               secure, efficient, and scalable cloud environments. From cloud
               migration to security audits, we&lsquo;ve got you covered.
             </p>
-            <div className="sm:mt-5 font-mono mt-8 flex justify-center items-center  md:flex-wrap gap-4">
+            <div className="mt-8 flex items-center justify-center gap-4 font-mono md:flex-wrap sm:mt-5">
               <Button
                 asChild
-                className="w-fit md:w-full border border-black  flex items-center justify-center px-8 py-3  text-base font-medium rounded-full text-white bg-orange-600 hover:bg-orange-700 md:py-4 md:text-lg md:px-10"
+                className="flex w-fit items-center justify-center rounded-full border border-black bg-orange-600 px-8 py-3 text-base font-medium text-white hover:bg-orange-700 md:w-full md:px-10 md:py-4 md:text-lg"
               >
                 <Link href="#">
                   OUR SERVICES <ArrowIcon />
@@ -51,17 +51,17 @@ const Hero = () => {
               <Button
                 asChild
                 variant={"outline"}
-                className="w-fit md:w-full px-8 py-3 rounded-full text-base font-medium text-white md:py-4 md:text-lg md:px-10"
+                className="w-fit rounded-full px-8 py-3 text-base font-medium text-white md:w-full md:px-10 md:py-4 md:text-lg"
               >
                 <Link href="#" className="flex items-center justify-center">
-                  GET A FREE CONSULTATION <ArrowIcon />
+                  GET A FREE CONSULTATION. <ArrowIcon />
                 </Link>
               </Button>
             </div>
           </div>
         </>
       </header>
-      <div className="my-7 max-w-7xl mx-auto overflow-hidden">
+      <div className="mx-auto my-7 max-w-7xl overflow-hidden">
         <PartnersSlider />
       </div>
     </Fragment>
