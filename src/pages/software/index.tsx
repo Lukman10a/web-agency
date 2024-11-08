449ec04ccc91d7821fabb5f2d494ae810f7a55ef
Lukman10a <abdulrauflukman9@gmail.com>
2024-11-08 23:24:27 +0100

    update software page

--- src/pages/software/index.tsx
+++ src/pages/software/index.tsx
@@ -8,11 +8,28 @@ import SuccessStory from "@/components/success-story";
 import Resources from "@/components/resources";
 import SoftwareHero from "@/components/softwareHero";
 import { PartnersSlider } from "@/components/shared/partners-slider";
+import Marquee from "@/components/ui/animated/marquee";
+import Image from "next/image";
+import radar from "/public/svg/radar.svg";
+import space from "/public/svg/space.svg";
+import thrive from "/public/svg/thrive.svg";
+import magellan from "/public/svg/magellan.svg";
+import playbill from "/public/svg/playbill.svg";
 
 export default function Software() {
   return (
     <main>
       <SoftwareHero />
+      <Marquee className="overflow-hidden" applyMask pauseOnHover>
+        {[radar, space, thrive, magellan, playbill].map((brand, index) => (
+          <Image
+            key={index}
+            src={brand}
+            alt={`Brand ${index}`}
+            className="w-36"
+          />
+        ))}
+      </Marquee>
       <PartnersSlider />
       <SolutionsSection />
       <HelpSection />
