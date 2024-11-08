51286e785f8408d4e6251c11a5b053c8df2554da
Lukman10a <abdulrauflukman9@gmail.com>
2024-11-08 22:47:15 +0100

    cleanup

--- src/pages/software/index.tsx
+++ src/pages/software/index.tsx
@@ -8,18 +8,6 @@ import SuccessStory from "@/components/success-story";
 import Resources from "@/components/resources";
 import SoftwareHero from "@/components/softwareHero";
 import { PartnersSlider } from "@/components/shared/partners-slider";
-// import lightbox from "/public/svg/lightbox.svg";
-// import featherdev from "/public/svg/featherdev.svg";
-// import boltshift from "/public/svg/boltshift.svg";
-// import globalbank from "/public/svg/globalbank.svg";
-
-// const images = [
-//   lightbox.src,
-//   featherdev.src,
-//   globalbank.src,
-//   boltshift.src,
-//   globalbank.src,
-// ];
 
 export default function Software() {
   return (
