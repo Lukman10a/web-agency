d45c216460808deb697f5d1b89ec247927ac4909
sheriff <ajibolasheriffdeen02@gmail.com>
2024-11-10 01:21:11 +0100

    made adjustments

--- src/pages/software/teverse-control.tsx
+++ src/pages/software/teverse-control.tsx
@@ -1,18 +1,7 @@
-import Benefits from "@/components/benefits";
-import CardSection from "@/components/card-section";
-import NewHero from "@/components/shared/new-hero";
-import WhyTeverse from "@/components/why-teverse";
 import React from "react";
 
 const TeverseControl = () => {
-  return (
-    <div>
-      <NewHero />
-      <WhyTeverse />
-      <CardSection />
-      <Benefits />
-    </div>
-  );
+  return <div>TeverseControl</div>;
 };
 
 export default TeverseControl;
