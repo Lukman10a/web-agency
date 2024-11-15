74cd36a44a46164bd0ea769d8da335234c871de9
sheriff <ajibolasheriffdeen02@gmail.com>
2024-11-15 11:17:18 +0100

    FAQ redesigned

--- src/components/shared/FAQ.tsx
+++ src/components/shared/FAQ.tsx
@@ -27,17 +27,10 @@ const faqData: FAQItem[] = [
 
 const FAQ: React.FC = () => {
   return (
-<<<<<<< HEAD:src/components/FAQ.tsx
     <section className="mx-auto mb-24 w-[85%] max-w-[1200px] rounded-2xl bg-[#FBFBFB] py-16 xl:w-full xl:rounded-none sm:mb-16">
       <div className="mx-auto w-[90%]">
         <h2 className="mb-4 text-center font-sora text-4xl font-medium text-gray-900 sm:text-2xl">
           Frequently asked question
-=======
-    <section className="w-full bg-white py-16">
-      <div className="mx-auto max-w-4xl px-4 lg:px-8 sm:px-6">
-        <h2 className="text-center text-5xl font-extrabold text-gray-900 md:text-4xl sm:text-3xl">
-          FREQUENTLY ASKED QUESTIONS
->>>>>>> main:src/components/shared/FAQ.tsx
         </h2>
         <FAQSection faqData={faqData} />
       </div>
