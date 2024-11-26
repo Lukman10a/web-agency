5f27b50e4984812c09adbc8b51d45b8f3fb5c204
luqmaan <obalolaluqman16@gmail.com>
2024-11-26 12:12:21 +0100

    responsiveness

--- src/components/book-demo.tsx
+++ src/components/book-demo.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 const BookDemoSection: React.FC = () => {
   return (
-    <section className="flex sm:flex-col items-center py-14 max-w-7xl max-h-[32rem]">
+    <section className="flex sm:flex-col items-center sm:mb-10 py-14 sm:py-0 max-w-7xl">
       {/* Image Placeholder */}
       
-      <div className="w-1/2 rounded-tl-3xl rounded-bl-3xl sm:w-full sm:px-4 flex items-center mb-0 justify-end md:justify-center px-2">
+      <div className="w-1/2 rounded-tl-3xl sm:rounded-tl-none sm:rounded-bl-none rounded-bl-3xl sm:w-full sm:px-4 flex items-center mb-0 justify-end md:justify-center px-2">
         <Image
           src="/assets/checkers.png"
           height={600}
@@ -18,14 +18,14 @@ const BookDemoSection: React.FC = () => {
       
 
       {/* Text Content */}
-      <div className=" w-5/12 sm:w-full text-left pl-12 sm:px-4 rounded-tr-3xl rounded-br-3xl bg-slate-300 min-h-[31rem] py-14">
+      <div className=" w-5/12 sm:w-full text-left pl-12 sm:px-4 rounded-tr-3xl rounded-br-3xl bg-slate-300 min-h-[31rem] py-14 sm:py-4">
         {/* Badge */}
-        <div className="text-sm font-bold text-black tracking-wide px-3 py-1 text-left ml-0 sm:mt-5 border border-gray-300 rounded-full inline-block mb-4">
+        <div className="text-sm font-bold text-black tracking-wide py-1 text-left ml-0 sm:mt-5 border border-gray-300 rounded-full inline-block mb-4">
           BOOK YOUR DEMO
         </div>
 
         {/* Title */}
-        <h2 className="text-2xl sm:text-xl md:text-xl font-bold w-5/6 text-black mb-4">
+        <h2 className="text-2xl sm:text-lg font-bold w-5/6 text-black mb-4">
         We have the tools to understand your cloud and the guidance to make the most of it.
           
         </h2>
@@ -37,7 +37,7 @@ const BookDemoSection: React.FC = () => {
         </p>
 
         {/* Button */}
-        <button className="px-6 py-3 text-xl bg-black text-white font-medium rounded-full hover:bg-orange-600 transition duration-300">
+        <button className="px-6 py-3 text-xl sm:text-sm bg-black text-white font-medium rounded-full hover:bg-orange-600 transition duration-300">
           Book your demo
         </button>
       </div>
--- src/components/software-faq.tsx
+++ src/components/software-faq.tsx
@@ -33,7 +33,7 @@ const SoftwareFAQ: React.FC = () => {
       //   background: "linear-gradient(61.83deg, #FBFBFB 32.56%, #FFFFFF 100%)",
       // }}
     >
-      <div className="mx-auto max-w-6xl  py-10  flex flex-col lg:px-8 sm:px- space-y-6 border-2 border-black rounded-3xl">
+      <div className="mx-auto max-w-6xl  py-10  sm:py-4 flex flex-col sm:px-4 space-y-6 border-2 border-black rounded-3xl">
         <h2 className="text-center text-3xl font-extrabold text-gray-900 md:text-xl sm:text-lg border-b-2 border-black">
           FREQUENTLY ASKED QUESTIONS
         </h2>
