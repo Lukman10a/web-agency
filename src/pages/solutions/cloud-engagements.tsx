b0c4d25264573f9fd7ac86eb2a2b2cf7184e5777
Lukman10a <abdulrauflukman9@gmail.com>
2024-11-30 07:29:45 +0100

    ...

--- src/pages/solutions/cloud-engagements.tsx
+++ src/pages/solutions/cloud-engagements.tsx
@@ -56,7 +56,7 @@ export default function CloudEngagements() {
         tag
         title="DevOps Excellence, Visualized"
         mainDescription="Mission Cloud Engagements - DevOps, powered by Mission Control, enables both you and Mission to easily track and manage the work accomplished by your Mission Cloud DevOps team. Visualize the status of your initiatives in real time and use Mission Control’s observability and reporting tools to generate and prioritize a backlog of work while understanding its impact on your alignment to best practices."
-        additionalDescription="With Engagements, our aim is simple: make the experience of accomplishing your projects with Mission that much richer, more transparent, and accountable, so you can get your DevOps initiatives built to spec and launched quickly"
+        additionalDescription="With Engagements, our aim is simple: make the experience of accomplishing your projects with Mission that much richer, more transparent, and accountable, so you can get your DevOps initiatives built to spec and launched quickly."
       />
       <CardSection />
       <section className="my-16 grid grid-cols-2 p-20 md:grid-cols-1 md:p-12 sm:p-8">
